77df8103d57bae20eda7f4141655dc48413e2e82
Tammer Galal <tg385@scarletmail.rutgers.edu>
2016-01-13 17:02:31 -0500

    initial files createded and required text added.

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'j2nRddxWDphr6Tg1NWfKWnOgO',
+  consumer_secret: '4Etdi6D13LnlXL0QSp0ns6QzPkXrUH6413ebMPhggpHXv5DK2i',
+  access_token_key: '754181970-hhaimDpWfftRv09IgpYjNEn4loiuMUQq7GODDmPL',
+  access_token_secret: 'kqj7wVOymjeW6eshiMoFQix0Bj4XN6SdD6QDnK3DinhV4',
+}
---
+++ liri.js
@@ -0,0 +1,3 @@
+var items = 
+
+console.log(keys.js);
