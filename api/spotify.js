9702ef7c8f9ebe96512f5a1af35f70f24928ca9a
tammerg <tg385@scarletmail.rutgers.edu>
2016-01-21 18:20:21 -0500

    got the spotify function working and console logging the data wanted

--- api/spotify.js
+++ api/spotify.js
@@ -1,14 +1,16 @@
 module.exports = function spotify(searchQuery){
 var track = searchQuery;
-var spotify = require("spotify");
+var spotify = require("spotify")
 
  
 spotify.search({ type: 'track', query: track }, function(err, data) {
     if ( err ) {
-        console.log('Error occurred: ' + err);    
+      console.log('Error occurred: ' + err);    
     }
     else {
-      console.log(JSON.parse(data));
+      console.log("preview url - " + data.tracks.items[0].preview_url);
+      console.log("album name - " + data.tracks.items[0].album.name);
+      console.log("artist name - "+ data.tracks.items[0].artists[0].name);
     }
   });
 }
